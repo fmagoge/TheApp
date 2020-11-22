2cd6a413feb9c18e6b9e38d6db8f33602dfec49d
fmagoge <fmagoge@gmail.com>
2020-11-23 01:42:48 +0200

    Display User List From Firestore Database

--- app/src/main/java/com/dmatrix/theapp/activities/MainActivity.java
+++ app/src/main/java/com/dmatrix/theapp/activities/MainActivity.java
@@ -2,24 +2,36 @@ package com.dmatrix.theapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dmatrix.theapp.R;
+import com.dmatrix.theapp.adapters.UsersAdapter;
+import com.dmatrix.theapp.models.User;
 import com.dmatrix.theapp.utilities.Constants;
 import com.dmatrix.theapp.utilities.PrefereneManager;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private PrefereneManager prefereneManager;
+    private List<User> users;
+    private UsersAdapter usersAdapter;
+    private TextView textErrorMessage;
+    private ProgressBar usersProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,51 @@ public class MainActivity extends AppCompatActivity {
                 sendFCMTokenToDatabase(task.getResult().getToken());
             }
         });
+
+        RecyclerView usersRecyclerView = findViewById(R.id.userRecyclerView);
+        textErrorMessage = findViewById(R.id.textErrorMessage);
+        usersProgressBar = findViewById(R.id.usersProgressBar);
+
+        users = new ArrayList<>();
+        usersAdapter = new UsersAdapter(users);
+        usersRecyclerView.setAdapter(usersAdapter);
+
+        getUsers();
+    }
+
+    private void getUsers(){
+        usersProgressBar.setVisibility(View.VISIBLE);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USES)
+                .get()
+                .addOnCompleteListener(task -> {
+                    usersProgressBar.setVisibility(View.GONE);
+                    String myUserId = prefereneManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null){
+                        for(QueryDocumentSnapshot documentSnapshot: task.getResult()){
+                            if(myUserId.equals(documentSnapshot.getId())){
+                                continue; //So as to skip current user
+                            }else {
+                                User user = new User();
+                                user.firstName = documentSnapshot.getString(Constants.KEY_FIRST_NAME);
+                                user.lastName = documentSnapshot.getString(Constants.KEY_LAST_NAME);
+                                user.email = documentSnapshot.getString(Constants.KEY_EMAIL);
+                                user.token = documentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                                users.add(user);
+                            }
+
+                            if (users.size()>0){
+                                usersAdapter.notifyDataSetChanged();
+                            }else {
+                                textErrorMessage.setText(String.format("%s", "No users available"));
+                                textErrorMessage.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }else {
+                        textErrorMessage.setText(String.format("%s", "No users available"));
+                        textErrorMessage.setVisibility(View.VISIBLE);
+                    }
+                });
     }
 
     private void sendFCMTokenToDatabase(String token){
@@ -51,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
                         prefereneManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(aVoid -> Toast.makeText(MainActivity.this, "Token update successfully",Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Unable to send token: "+e.getMessage(),Toast.LENGTH_SHORT).show());
     }
 
---
+++ app/src/main/java/com/dmatrix/theapp/models/User.java
@@ -0,0 +1,5 @@
+package com.dmatrix.theapp.models;
+
+public class User {
+    public String firstName, lastName, email, token;
+}
